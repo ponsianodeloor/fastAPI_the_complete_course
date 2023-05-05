2bc1a869db72f65f454edfef330ffd27cae2b77a
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-05-04 23:41:21 -0500

    Boolean and operators

---
+++ PythonRefresher/008_boolean_and_operators/001_boolean_and_operators.py
@@ -0,0 +1,23 @@
+like_coffee = True
+like_tea = False
+
+favorite_food = "Pizza"
+favorite_number = 89
+
+print(type(like_coffee))
+print(type(favorite_food))
+print(type(favorite_number))
+
+print(1 == 2)
+print(1 != 2)
+print(1 > 2)
+print(1 < 2)
+print(1 >= 1)
+print(1 <= 2)
+
+# Logical operators
+
+print(1 > 3 and 5 < 7)
+print(1 > 3 or 5 < 7)
+print(1 == 1)
+print(not (1 == 1))
