e3d9858fde3866e3247450f312535894e1a2bb56
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-05-09 05:59:03 -0500

    if elif else

---
+++ PythonRefresher/009_if_else/001_if_else.py
@@ -0,0 +1,13 @@
+"""
+Flow control: If ElseIf
+"""
+
+
+x = 0
+
+if x > 1:
+    print(f"The value of {x} is greater than 1")
+else:
+    print(f"The value of {x} is not greater than 1")
+
+print("Fuera de la condicion")
---
+++ PythonRefresher/009_if_else/002_if_else_hour.py
@@ -0,0 +1,13 @@
+"""
+Flow control: If ElseIf
+"""
+
+
+hour = 21
+
+if hour < 15:
+    print(f"Good morning")
+elif hour < 20:
+    print("Good Afternoon")
+else:
+    print(f"Good Night")
