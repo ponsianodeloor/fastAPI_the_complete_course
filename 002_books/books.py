53aba13fd728097c171eb22385bc0f875bbfe63c
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-06-08 10:48:55 -0500

    crear un diccionario de libros para mostrarlos en la ruta read all books

--- 002_books/books.py
+++ 002_books/books.py
@@ -2,7 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+BOOKS = [
+    {'title': 'Title one', 'author': 'Author one', 'category': 'Compute'},
+    {'title': 'Title two', 'author': 'Author two', 'category': 'Compute'},
+    {'title': 'Title three', 'author': 'Author three', 'category': 'History'},
+    {'title': 'Title four', 'author': 'Author four', 'category': 'History'},
+    {'title': 'Title five', 'author': 'Author five', 'category': 'Math'},
+    {'title': 'Title six', 'author': 'Author six', 'category': 'Math'},
+]
 
-@app.get("/")
-async def first_api():
-    return {'message': 'hi Ponsiano'}
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
