576da3c4742cbee3966bccf01f2b928a9416a2bf
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-06-12 13:52:49 -0500

    actualizacion por titulo del array de libros

--- 002_books/books.py
+++ 002_books/books.py
@@ -53,3 +53,11 @@ async def read_book_by_author_and_category(author: str, category: str):
 async def create_book(new_book=Body()):
     BOOKS.append(new_book)
     return {'message': 'Libro creado correctamente'}
+
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+            return {'message': 'Libro actualizado correctamente'}
