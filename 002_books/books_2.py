086b62dd0389c4535c10075e00f506ffd8cfe4ff
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-06-19 14:43:25 -0500

    parametros opcionales con pydantic

--- 002_books/books_2.py
+++ 002_books/books_2.py
@@ -22,9 +22,9 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id: int
+    id: Optional[int]
     title: str = Field(min_length=3)
-    author: str = Field(min_length=1)
+    author: str = Field(min_length=10)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
 
