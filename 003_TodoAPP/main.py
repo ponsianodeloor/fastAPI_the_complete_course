8ceb09d1996907f327915832181c761791475a00
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-07-04 14:52:34 -0500

    asignar el valor de la variable Annottaed para uso en proximas rutas y finalizar el CRUD

--- 003_TodoAPP/main.py
+++ 003_TodoAPP/main.py
@@ -19,7 +19,10 @@ def get_db():
         db.close()
 
 
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
 @app.get("/")
-async def read_all(db: Annotated[Session, Depends(get_db)]):
+async def read_all(db: db_dependency):
     return db.query(Todos).all()
 
