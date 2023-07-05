d6144fabd917c1b88f4c9a5d104689f964ac6138
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-07-05 10:00:03 -0500

    busqueda de todo por id

--- 003_TodoAPP/main.py
+++ 003_TodoAPP/main.py
@@ -2,7 +2,7 @@ from typing import Annotated
 from sqlalchemy.orm import Session
 
 from fastapi.responses import RedirectResponse
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
 from starlette import status
 
 import models
@@ -33,4 +33,13 @@ def go_to_docs():
 @app.get("/read_all", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
+
+
+@app.get("/todo/{id}", status_code=status.HTTP_200_OK)
+async def read_id(db: db_dependency, id: int = Path(gt=0)):
+    todo = db.query(Todos).filter(Todos.id == id).first()
+    if todo is not None:
+        return todo
+    raise HTTPException(status_code=404, detail='Todo no encontrado')
+
 
