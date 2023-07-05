1bdecf4342edadd581cf66d16707f559277693f0
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-07-05 09:45:12 -0500

    mostrar todos los registros de Todo con db.query(Model).all()

--- 003_TodoAPP/main.py
+++ 003_TodoAPP/main.py
@@ -1,7 +1,10 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
 
+from fastapi.responses import RedirectResponse
 from fastapi import FastAPI, Depends
+from starlette import status
+
 import models
 from models import Todos
 from database import engine, SessionLocal
@@ -23,6 +26,11 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 
 @app.get("/")
+def go_to_docs():
+    return RedirectResponse("/docs")
+
+
+@app.get("/read_all", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
 
