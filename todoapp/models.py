959c50bf6c13a57e7343e808c46da09c560cc701
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-07-08 20:37:38 -0500

    creacion del modelo user

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,6 +1,19 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from pydantic import BaseModel, Field
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
 
 
 class Todos(Base):
@@ -11,6 +24,7 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey("users.id"))
 
 
 class TodosRequest(BaseModel):
