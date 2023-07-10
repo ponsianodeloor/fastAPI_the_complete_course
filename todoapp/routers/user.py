524a95c00849584c5c244516dcb5f9c26b490b8a
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-07-09 21:22:45 -0500

    decodificar JWT

--- todoapp/routers/user.py
+++ todoapp/routers/user.py
@@ -1,15 +1,15 @@
 from datetime import timedelta, datetime
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from starlette import status
 from schemas import user, token
 from database import get_db
 from models import User
 from passlib.context import CryptContext
 
-from fastapi.security import OAuth2PasswordRequestForm
-from jose import jwt
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt, JWTError
 
 router = APIRouter()
 
@@ -17,6 +17,7 @@ SECRET_KEY = '197b2c37c391bed93fe80344fe73b806947a65e36206e05a1a23c2fa12702fe3'
 ALGORITHM = 'HS256'
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
@@ -69,6 +70,22 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
+async def get_user_by_token(user_token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(user_token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('user_id')
+
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
+        return {
+            'username': username,
+            'user_id': user_id
+        }
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
+
+
 @router.post("/token", response_model=TokenSchema)
 async def login_for_access_token(
         form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
@@ -77,7 +94,7 @@ async def login_for_access_token(
     login_user = auth_user(form_data.username, form_data.password, db)
 
     if not login_user:
-        return 'Failed Authentication'
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
 
     get_token = create_access_token(login_user.username, login_user.id, timedelta(minutes=20))
 
